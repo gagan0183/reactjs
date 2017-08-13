fdf5fbba95282b261663f79b44419774f6535390
gagan0183 <g.deepsingh1@gmail.com>
2017-08-14 00:58:59 +0530

    creating sep components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+class Button extends Component {
 
+  handleClick = () => {
+    this.props.incrementClics(this.props.increment) ;
+  };
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        +{this.props.increment}
+      </button> 
+    );
+  }
+}
+
+const Result = (props) => {
+  return (
+    <div>{props.counter}</div>
+  );
+};
+
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -10,17 +30,19 @@ class App extends Component {
     };
   }
 
-  handleClick = () => {
+  incrementCounts = (counts) => {
     this.setState({
-      counter: this.state.counter + 1
+      counter: this.state.counter + counts
     });
   };
 
   render() {
     return (
-      <button onClick={this.handleClick}>
-        {this.state.counter}
-      </button> 
+      <div>
+        <Button increment = {1} incrementClics = {this.incrementCounts} />
+        <Button increment = {3} incrementClics = {this.incrementCounts} />
+        <Result counter={this.state.counter}/>
+      </div>
     );
   }
 }
