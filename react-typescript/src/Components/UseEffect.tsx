48ceec0ce3df2722e9bf864edd961e18c9213db0
Gagan <g.deepsingh1@gmail.com>
2021-06-16 20:07:44 +0530

    use effect dependencies

--- react-typescript/src/Components/UseEffect.tsx
+++ react-typescript/src/Components/UseEffect.tsx
@@ -7,13 +7,13 @@ const UseEffect: React.FC = props => {
     useEffect(() => {
         document.title = `Counter at ${counter1}`;
         console.log('Effect called');
-    });
+    }, [counter1]);
 
     return <>
         <div>Counter 1: {counter1}</div>
         <div>Counter 2: {counter2}</div>
-        <button onClick={() => setCounter1(c => c + 1)}>Increment</button>
-        <button onClick={() => setCounter2(c => c - 1)}>Decrement</button>
+        <button onClick={() => setCounter1(c => c + 1)}>Increment Counter 1</button>
+        <button onClick={() => setCounter2(c => c + 1)}>Increment Counter 2</button>
     </>
 }
 
